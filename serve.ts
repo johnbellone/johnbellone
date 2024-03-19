47cd2d91df346165c63383b4bd791e6fde55e6b4
John Bellone <me@jbellone.dev>
2024-03-18 23:08:41 -0400

    lume: remove middleware

--- serve.ts
+++ serve.ts
@@ -1,15 +1,9 @@
 import Server from "lume/core/server.ts";
-import notFound from "lume/middlewares/not_found.ts";
 
 const server = new Server({
   port: 8000,
   root: `${Deno.cwd()}/_site`,
 });
-
-server.use(notFound({
-  root: `${Deno.cwd()}/_site`,
-  page404: "/404/",
-}));
 
 server.start();
 
